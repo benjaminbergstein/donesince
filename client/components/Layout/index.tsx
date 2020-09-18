40639440480237926bfa5a7e43ee965180f0f10b
Benjamin Bergstein <bennyjbergstein+github@gmail.com>
2020-09-18 22:23:08 +0000

    Fix "🐞 Screen appears blank when scrolling from bottom of day with long timeline  to shorter timeline day."

--- client/components/Layout/index.tsx
+++ client/components/Layout/index.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { Reset } from 'styled-reset'
 
 import System from '../../system/System'
+import Box from '../../system/Box'
+
 import styled from 'styled-components'
 import Flash, { FlashProvider } from '../../components/Flash'
 import { DeviceProvider } from '../../contexts/DeviceContext'
@@ -10,13 +12,28 @@ import { DeviceProvider } from '../../contexts/DeviceContext'
 import Navigation from './Navigation'
 
 const Page = styled.div`
-  font-family: "Helvetica Neue", sans-serif
+  font-family: "Helvetica Neue", sans-serif;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
+
+const globalStyles = `
+html, body {
+  height: 100%;
+  min-height: 100%;
+}
+
+#__next {
+  height: 100%;
+}
 `
 
 const Layout: React.FC<{}> = ({ children }) => (
   <Page>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <style>{globalStyles}</style>
     </Head>
 
     <System>
@@ -26,7 +43,12 @@ const Layout: React.FC<{}> = ({ children }) => (
           <Navigation />
           <Flash />
 
-          {children}
+          <Box
+            flex="1"
+            overflow="scroll"
+          >
+            {children}
+          </Box>
         </DeviceProvider>
       </FlashProvider>
     </System>
--- client/components/Timeline/index.tsx
+++ client/components/Timeline/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useLayoutEffect, useRef, useState } from 'react'
 
 import SwipeableViews from 'react-swipeable-views';
 import Timeline from './Timeline'
 import SearchActivityTypes from '../SearchActivityTypes'
+
+import Box from '../../system/Box'
 
 const OffsetsToShow: number = 20
 
@@ -14,28 +16,47 @@ const shouldShowOffset: (offset: number, showingOffset: number) => boolean =
 
 const Wrapper: React.FC<{}> = () => {
   const [showingOffset, setShowingOffset] = React.useState<number>(OffsetsToShow - 1)
+  const [height, setHeight] = useState<string>('auto')
+  const boxRef = useRef<HTMLDivElement | null>(null)
+
+  useLayoutEffect(() => {
+    const listener = () => {
+      const { current: boxElement } = boxRef
+      if (boxElement === null) return
+      setHeight(boxElement.offsetHeight.toString() + 'px')
+    }
+
+    listener()
+    window.addEventListener('resize', listener)
+
+    return () => {
+      window.removeEventListener('resize', listener)
+    }
+  }, [])
 
   const offsets = Array(OffsetsToShow).fill(OffsetsToShow).map(
     (_, i)  => OffsetsToShow - i - 1
   )
 
-  return <>
+  return <Box flex="1" display="flex" flexDirection="column" overflowY="hidden">
     <SearchActivityTypes />
 
-    <SwipeableViews
-      index={offsets.length - 1}
-      enableMouseEvents={true}
-      onSwitching={(index) => { setShowingOffset(Math.ceil(index)) }}
-    >
-      {offsets.map((offset) => (
-        <>
-          {shouldShowOffset(offset, offsets[showingOffset]) && (
-            <Timeline key={`timeline-offset-${offset}`} offset={offset} />
-          )}
-        </>
-      ))}
-    </SwipeableViews>
-  </>
+    <Box ref={boxRef} display="flex" flex="1" overflowY="hidden">
+      <SwipeableViews
+        index={offsets.length - 1}
+        enableMouseEvents={true}
+        onSwitching={(index) => { setShowingOffset(Math.ceil(index)) }}
+      >
+        {offsets.map((offset) => (
+          <Box key={`swipable-view-${offset}`} height={height} overflowY="scroll" overflowX="hidden">
+            {shouldShowOffset(offset, offsets[showingOffset]) && (
+              <Timeline key={`timeline-offset-${offset}`} offset={offset} />
+            )}
+          </Box>
+        ))}
+      </SwipeableViews>
+    </Box>
+  </Box>
 }
 
 export default Wrapper
