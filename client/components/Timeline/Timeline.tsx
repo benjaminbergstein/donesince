de1ae6b04c1deb40dca94c2a18994770c39925f5
Benjamin Bergstein <bennyjbergstein+github@gmail.com>
2020-09-20 07:05:08 +0000

    Server and client use the same timezone.

--- client/components/Timeline/Timeline.tsx
+++ client/components/Timeline/Timeline.tsx
@@ -6,17 +6,17 @@ import Box from '../../system/Box'
 import Text from '../../system/Text'
 import ActivityLine from './ActivityLine'
 
-import { formatDatestamp } from '../../utils/time'
-
 interface Props {
   date: Date
 }
 
 const Timeline: React.FC<Props> = ({ date }) => {
-  const datestamp = formatDatestamp(date)
+  const [month, day, year] = date.toLocaleDateString('en-us', { month: '2-digit', day: '2-digit', year: 'numeric', timeZone: 'America/Los_Angeles' }).split("/")
+  const datestamp = [year, month, day].join("-")
+  console.log(datestamp)
   const { data: { timeline } } = useTimeline(datestamp)
 
-  const humanReadableDate = date.toLocaleDateString('en-us', { month: 'short', day: 'numeric', weekday: 'short' })
+  const humanReadableDate = date.toLocaleDateString('en-us', { month: 'short', day: 'numeric', weekday: 'short', timeZone: 'America/Los_Angeles' })
 
   return <Box minHeight="100%" overflowY="auto" overflowX="hidden">
     <Box
